ec7cc2dcc80302c4c8b9dd23c1d583acdad809b3
md-mostafa-khan <mdmosafakhanbd@gmail.com>
2024-06-23 18:38:11 +0600

    Exam Week-3

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $cookieMinutes = 1;
+        $cookiePath = '/';
+        $cookieDomain = $_SERVER['SERVER_NAME'];
+        $cookieSecure = false;
+        $cookieHttpOnly = true;
+
+        $response = new Response(json_encode($data), 200);
+
+        $response->cookie($cookieName, $cookieValue, $cookieMinutes, $cookiePath, $cookieDomain, $cookieSecure, $cookieHttpOnly);
+
+        return $response;
+        
+    }
+}
